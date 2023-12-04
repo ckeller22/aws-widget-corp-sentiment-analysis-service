5f4f2ef28895f911e7d3eba63bd21945420412f1
ckeller22 <chris.keller.dev@gmail.com>
2023-12-03 20:30:29 -0600

    feat: add simple blueprint, sync code volume

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,11 @@ from config import Config
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
-    @app.route('/')
+    @app.route('/whale')
     def test_page():
         return '<h1>Whale, Hello there!</h1>'
 
