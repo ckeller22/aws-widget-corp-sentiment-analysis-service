0fabb48501931e3a9fe162d577f283d83c370e81
ckeller22 <chris.keller.dev@gmail.com>
2023-12-05 15:15:12 -0600

    feat: add PredictionResponse model and update routes.py and test_predict.py

--- app/predict/routes.py
+++ app/predict/routes.py
@@ -2,14 +2,19 @@ from flask import request, jsonify, Response
 from app.predict import bp
 from pydantic import ValidationError
 from app.models.PredictionRequest import PredictionRequest
+from app.models.PredictionResponse import PredictionResponse
 
 
 @bp.route("/predict", methods=["POST"])
 def predict():
     try:
-        payload = PredictionRequest.model_validate_json(request.json)
+        payload = PredictionRequest.model_validate(request.json)
     except ValidationError as e:
         return {
             "message": "The request body does not match the expected schema",
             "errors": e.errors(),
         }, 400
+
+    response = PredictionResponse(sentiment="positive")
+
+    return response.model_dump()
--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,3 +1,7 @@
+from app.models.PredictionResponse import PredictionResponse
+from pydantic import ValidationError
+
+
 def test_bad_request_if_request_schema_invalid(client):
     payload = {"bad_key": "bad_value"}
     response = client.post("/predict", json=payload)
@@ -7,3 +11,11 @@ def test_bad_request_if_request_schema_invalid(client):
         response.get_json()["message"]
         == "The request body does not match the expected schema"
     )
+
+
+def test_response_returns_prediction_response_type(client):
+    payload = {"input": "This movie was very good."}
+    response = client.post("/predict", json=payload)
+
+    assert response.status_code == 200
+    assert PredictionResponse.model_validate(response.get_json())
