21b338704c0daa87eca3f734212c18f8b51c4495
ckeller22 <chris.keller.dev@gmail.com>
2023-12-05 22:08:15 -0600

    feat: add sentiment analysis functionality and bag of words feature

--- app/services/prediction_service.py
+++ app/services/prediction_service.py
@@ -3,6 +3,9 @@ import os
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.lancaster import LancasterStemmer
+from nltk import everygrams
+from string import punctuation as punctuation_list
+from nltk.tokenize import word_tokenize
 
 
 class PredictionService:
@@ -10,8 +13,10 @@ class PredictionService:
         self.load_model()
         self.load_nltk()
 
-    def predict(self, data):
-        pass
+    def get_sentiment(self, input_string):
+        words = self.extract_features(input_string)
+        words = self.bag_of_words(words)
+        return self.model.classify(words)
 
     def load_model(self):
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -36,3 +41,28 @@ class PredictionService:
 
         self.stopword_list = stopwords.words("english")
         self.stemmer = LancasterStemmer()
+
+    def extract_features(self, input_string):
+        """Extract features from the input string for sentiment analysis."""
+        # Tokenize words.
+        words = word_tokenize(input_string)
+
+        # Second pass, remove stop words and punctuation.
+        features = [
+            self.stemmer.stem(word)
+            for word in words
+            if self.stemmer.stem(word) not in self.stopword_list
+            and self.stemmer.stem(word) not in punctuation_list
+        ]
+
+        # Third pass, generate n_grams
+        n_grams = everygrams(features, max_len=3)
+
+        return n_grams
+
+    def bag_of_words(self, words):
+        """Create a bag of words from the input words."""
+        bag = {}
+        for word in words:
+            bag[word] = bag.get(word, 0) + 1
+        return bag
--- tests/services/test_prediction_service.py
+++ tests/services/test_prediction_service.py
@@ -17,7 +17,20 @@ def test_service_should_load_nltk_resources(service):
     assert service.stemmer is not None
 
 
-def test_service_should_return_positive_sentiment(service):
-    data = {"review": "I love this movie"}
-    result = service.predict(data)
-    assert result["sentiment"] == "positive"
+def test_get_sentiment_should_return_sentiment(service):
+    data = "Avatar The Last Airbender is the a movie that should have never been made. It is a disgrace to the original series."
+    result = service.get_sentiment(data)
+    assert isinstance(result, str)
+
+
+def test_bag_of_words_should_return_empty_dict_when_given_empty_list(service):
+    words = []
+    result = service.bag_of_words(words)
+    assert result == {}
+
+
+def test_bag_of_words_should_return_correct_bag_of_words(service):
+    words = ["apple", "banana", "apple", "cherry"]
+    result = service.bag_of_words(words)
+    expected = {"apple": 2, "banana": 1, "cherry": 1}
+    assert result == expected
